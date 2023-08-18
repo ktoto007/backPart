d3cfc512537c405fac259fd68327ef06e2154807
Streltsov Valerii <st.valera04@gmail.com>
2023-08-18 13:02:10 +0300

    Initial commit

---
+++ controlers/auth/index.js
@@ -0,0 +1,11 @@
+const { ctrlWrapper } = require("../../helpers");
+const login = require("/login");
+const register = require("/register");
+const updateUser = require("/updateUser");
+const logout = require("/logout");
+module.exports = {
+  login: ctrlWrapper(login),
+  register: ctrlWrapper(register),
+  updateUser: ctrlWrapper(updateUser),
+  logout: ctrlWrapper(logout),
+};
