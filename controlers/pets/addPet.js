cadfa9fca78b0dc8a6e483db64fcec7570ae77d1
Oleh Dashkevych <skoner@ukr.net>
2023-08-21 14:43:03 +0300

    changed add-pet route / delete-pet route / added petAvatar / added owner to mangoose schema

--- controlers/pets/addPet.js
+++ controlers/pets/addPet.js
@@ -1,10 +1,42 @@
-const { Pet } = require('../../models/pet');
+const { Pet } = require("../../models/pet");
+const path = require("path");
+const fs = require("fs/promises");
+
+const petPhotosDir = path.join(__dirname, "../", "../", "public", "petPhotos");
 
 const addPet = async (req, res) => {
-    const { _id: owner } = req.user;
-    // const date = new Date(req.body.date);
-    const result = await Pet.create({ ...req.body, owner });
-    res.status(201).json(result);
+
+  try {
+      const newPet = await Pet.create(
+          { ...req.body, owner: req.user._id },
+    );
+      
+    const { path: tempUpload, originalname } = req.file;
+
+  const filename = `${newPet._id}${originalname}`;
+  const resultUpload = path.join(petPhotosDir, filename);
+
+      await fs.rename(tempUpload, resultUpload);
+
+      const petAvatar = path.join("petPhotos", filename);
+   
+    const updatedPet = await Pet.findByIdAndUpdate(newPet._id, { avatar: petAvatar }, {
+      new: true,
+      })
+      console.log(updatedPet);
+
+      res.status(201).json({
+        name: newPet.name,
+        date: newPet.date,
+        type: newPet.type,
+        avatar: petAvatar,
+        id: newPet._id,
+        comments: newPet.comments,
+    });
+  } catch (error) {
+    console.error("Update petAvatar error:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 
 module.exports = addPet;
