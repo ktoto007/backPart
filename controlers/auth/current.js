9ff5e6c6fe1ff20af9a80f6780c512f905f0afc5
Streltsov Valerii <st.valera04@gmail.com>
2023-08-30 22:06:33 +0300

    fix for frontend

--- controlers/auth/current.js
+++ controlers/auth/current.js
@@ -1,8 +1,8 @@
 const current = async (req, res) => {
-  const { name, email, avatar, _id } = req.user;
+  const { name, email, avatar, _id, phone, birthday, city } = req.user;
 
   res.json({
-    user: { name, email, avatar, id: _id },
+    user: { name, email, avatar, id: _id, phone, birthday, city },
   });
 };
 
